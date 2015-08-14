993cacf60c13dfa9e61ab481e1e68d1a3d2bc4a2
Karel Tucek <kareltucek@centrum.cz>
2015-08-14 23:59:15 +0200

    build formalities

--- unit_test3/output_xml.h
+++ unit_test3/output_xml.h
@@ -6,9 +6,7 @@
 #include <emmintrin.h>
 void test_body()
 {
-  for(int r = 0;
-  r < 10000;
-  ++r)
+  for(int r = 0;r < 10000;++r)
   {
     std::srand(std::time(0));
     unsigned bsum = 0;
@@ -29,9 +27,7 @@ void test_body()
     unsigned size_out_0 = 0;
     unsigned processed_out_0 = 0;
     int * data_out_0 = NULL;
-    for (int i = 0;
-    i < 100 ;
-    i++)
+    for (int i = 0;i < 100 ;i++)
     {
       if ( pos_in_2 == size_in_2 )
       {
@@ -39,9 +35,7 @@ void test_body()
         if(data_in_2 != NULL)delete [] data_in_2;
         data_in_2 = new int[size_in_2];
         pos_in_2 = 0;
-        for(int i = 0;
-        i < size_in_2;
-        ++i)
+        for(int i = 0;i < size_in_2;++i)
         {
           data_in_2[i] = processed_in_2;
           processed_in_2 += 2;
@@ -53,9 +47,7 @@ void test_body()
         if(data_in_1 != NULL)delete [] data_in_1;
         data_in_1 = new int[size_in_1];
         pos_in_1 = 0;
-        for(int i = 0;
-        i < size_in_1;
-        ++i)
+        for(int i = 0;i < size_in_1;++i)
         {
           data_in_1[i] = processed_in_1;
           processed_in_1 += 1;
@@ -63,9 +55,7 @@ void test_body()
         }
       if ( pos_out_0 == size_out_0 )
       {
-        for(int i = 0;
-        i < size_out_0;
-        ++i)
+        for(int i = 0;i < size_out_0;++i)
         {
           //std::cout << 0 << " " << data_out_0[i] << std::endl;
           /*assert(data_out_0[i] == processed_out_0);
@@ -79,14 +69,10 @@ void test_body()
         }
       auto batch_size = std::min(std::min(std::min(std::numeric_limits<unsigned>::max(), size_in_2 - pos_in_2), size_in_1 - pos_in_1), size_out_0 - pos_out_0);
       std::size_t j = 0;
-      for ( ;
-      j + 4 <= batch_size;
-      j += 4)
+      for ( ;j + 4 <= batch_size;j += 4)
       {
         __m128i var_LDI_0_w4_0 = _mm_loadu_si128((const __m128i*)&data_in_1[pos_in_1+j]);
-        ;
         __m128i var_LDI_1_w4_0 = _mm_loadu_si128((const __m128i*)&data_in_2[pos_in_2+j]);
-        ;
         __m128i var_LDI_3_conv_w2_0 = var_LDI_1_w4_0;
         __m128i var_LDI_3_conv_w2_2 = _mm_srli_si128(var_LDI_1_w4_0,4);
         __m128i var_LDI_4_conv_w2_0 = var_LDI_0_w4_0;
@@ -97,21 +83,15 @@ void test_body()
         __m128i var_MULI_7_conv_w4_0 = _mm_unpacklo_epi32(var_MULI_2_w2_0,var_MULI_2_w2_2);
         __m128i var_ADDI_6_w4_0 = _mm_add_epi32(var_MULI_7_conv_w4_0, var_ADDI_5_w4_0);
         _mm_storeu_si128((__m128i*)&data_out_0[pos_out_0+j], var_ADDI_6_w4_0);
-        ;
         }
-      for (;
-      j < batch_size;
-      ++j)
+      for (;j < batch_size;++j)
       {
         int var_LDI_9_w1_0 = data_in_1[pos_in_1+j];
-        ;
         int var_LDI_10_w1_0 = data_in_2[pos_in_2+j];
-        ;
         int var_MULI_11_w1_0 = var_LDI_9_w1_0 * var_LDI_10_w1_0;
         int var_ADDI_12_w1_0 = var_LDI_10_w1_0 + var_LDI_9_w1_0;
         int var_ADDI_13_w1_0 = var_MULI_11_w1_0 + var_ADDI_12_w1_0;
         data_out_0[pos_out_0+j] = var_ADDI_13_w1_0;
-        ;
         }
       pos_in_2 += batch_size;
       pos_in_1 += batch_size;
